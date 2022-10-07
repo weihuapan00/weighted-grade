8ab6942198a9266c2cf60218737d972268a3a0f3
weihuapan00 <w5pan@ucsd.edu>
2022-10-07 12:22:21 -0700

    finish

--- Grade.java
+++ Grade.java
@@ -1,11 +1,17 @@
 public class Grade{
     // initialize the variable in the class
     private double grade = 0; //total weight grade
-    private int size = 9;
+    private int size = 8;
     private double[] percentage = new double[8];
     private double[] total_grade = new double[8];
     private double[] earned_grade = new double[8];
 
+    // default constructor
+    public Grade(){
+
+    }
+
+    // quick constructor that set percentage[], total_grade[], earned_grade[]
     public Grade(double[] percentage, double[] total_grade, double[] earned_grade){
         setPercentage(percentage);
         setTotal_grade(total_grade);
@@ -13,10 +19,10 @@ public class Grade{
     }
 
     //calculate the grade in one assignment and add to the total grade
-    public void calculateGrade(double total,double earned,double percent){
+    public void calculateGrade(){
         int temp = 0;
         for(int i=0; i< this.size; i++ ){
-            temp +=  earned / total * percent;
+            temp +=  earned_grade[i] / total_grade[i] * percentage[i];
         }
         setGrade(temp);
     }
@@ -29,30 +35,37 @@ public class Grade{
         
     }
 
+    //setter for grade
     public void setGrade(double grade) {
         this.grade = grade;
     }
 
+    //getter for percentage
     public double[] getPercentage() {
         return percentage;
     }
 
+    //setter for percentage
     public void setPercentage(double[] percentage) {
         this.percentage = percentage;
     }
 
+    //getter for total_grade
     public double[] getTotal_grade() {
         return total_grade;
     }
 
+    //setter for total_grade 
     public void setTotal_grade(double[] total_grade) {
         this.total_grade = total_grade;
     }
 
+    // getter for earned_grade
     public double[] getEarned_grade() {
         return earned_grade;
     }
 
+    //setter for earn_grade
     public void setEarned_grade(double[] earned_grade) {
         this.earned_grade = earned_grade;
     }
--- MyMain.java
+++ MyMain.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class MyMain{
 
@@ -5,31 +6,53 @@ public class MyMain{
         // create scanner obj
         Scanner sc = new Scanner(System.in);
 
-        //create grade obj
-        Grade myGrade = new Grade();
+        //init variable that use in grade class
+        double[] total_point = new double[8];
+        double[] earn_point = new double[8];
+        double[] percentage= new double[8];
+
+
 
         //loop 8 times to input 8 assignment from user
         for (int i=0; i<8; i++){
             // print out the prompts for user input
+            // and save the value to 3 array: total_point,earned_point, percentage
             System.out.println("Please enter total point: ");
             double total = sc.nextDouble();
-
+            total_point[i] = total;
 
             System.out.println("Please enter earned point: ");
             double earned = sc.nextDouble();
+            earn_point[i] = earned;
     
             System.out.println("Please enter assignment percentage(in float): ");
             double percent = sc.nextDouble();
-
-
-             
-            myGrade.calculateGrade(total,earned,percent);
-            System.out.println(myGrade.toString());
-            
+            percentage[i] = percent;
         }
-        // System.out.println("Please enter total point: ");
-        // double grade = sc.nextDouble();
+
         sc.close();// close scanner
+
+        // test case
+        // double[] total_point = {20, 30, 40, 50, 60, 100, 200, 300};
+        // double[] earn_point = {20,30,40,50,60,100,200,300};
+        // double[] percentage = {10, 10, 10, 10, 10, 10, 15, 25};
+        
+        //init grade obj
+        Grade myGrade = new Grade();
+
+        //set all the variable in order to calculate the grade
+        myGrade.setEarned_grade(earn_point);
+        myGrade.setPercentage(percentage);
+        myGrade.setTotal_grade(percentage);
+
+        // calculate the grade
+        myGrade.calculateGrade();
         System.out.println(myGrade.toString());
+
+        // test_code printout 3 arrays
+        // System.out.println(Arrays.toString(myGrade.getPercentage()));
+        // System.out.println(Arrays.toString(myGrade.getTotal_grade()));
+        // System.out.println(Arrays.toString(myGrade.getEarned_grade()));
+
     }
 }
