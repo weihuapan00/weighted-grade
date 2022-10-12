93fd0bfeae002314277bde1b3d957cf82d0fef2e
william <w5pan@ucsd.edu>
2022-10-12 14:29:27 -0700

    start lab4 change array to linkedlist

--- Grade.java
+++ Grade.java
@@ -1,18 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Grade{
     // initialize the variable in the class
     private double grade = 0; //total weight grade
-    private int size = 8;
-    private double[] percentage = new double[8];
-    private double[] total_grade = new double[8];
-    private double[] earned_grade = new double[8];
+    private int size = 0; // initit 
+    
+
+    private LinkedList<Double> percentage = new LinkedList<>();
+    private LinkedList<Double> total_grade = new LinkedList<>();
+    private LinkedList<Double> earned_grade = new LinkedList<>();
 
     // default constructor
     public Grade(){
-
+        
     }
 
     // quick constructor that set percentage[], total_grade[], earned_grade[]
-    public Grade(double[] percentage, double[] total_grade, double[] earned_grade){
+    public Grade(int length, double[] percentage, double[] total_grade, double[] earned_grade){
+        setSize(length);
         setPercentage(percentage);
         setTotal_grade(total_grade);
         setEarned_grade(earned_grade);
@@ -22,7 +29,7 @@ public class Grade{
     public void calculateGrade(){
         double temp = 0;
         for(int i=0; i< this.size; i++ ){
-            temp +=  earned_grade[i] / total_grade[i] * percentage[i];
+            temp +=  earned_grade.get(i) / total_grade.get(i) * percentage.get(i);
         }
         setGrade(temp);
     }
@@ -59,7 +66,7 @@ public class Grade{
     }
 
     //getter for percentage
-    public double[] getPercentage() {
+    public LinkedList<Double> getPercentage() {
         return percentage;
     }
 
@@ -69,7 +76,7 @@ public class Grade{
     }
 
     //getter for total_grade
-    public double[] getTotal_grade() {
+    public LinkedList<Double> getTotal_grade() {
         return total_grade;
     }
 
@@ -79,14 +86,24 @@ public class Grade{
     }
 
     // getter for earned_grade
-    public double[] getEarned_grade() {
+    public LinkedList<Double> getEarned_grade() {
         return earned_grade;
     }
 
     //setter for earn_grade
     public void setEarned_grade(double[] earned_grade) {
-        this.earned_grade = earned_grade;
+        this.earned_grade.clear();
+        Collections.addAll(this.earned_grade, earned_grade);
     }
-    
+
+    //getter for array size
+    public int getSize() {
+        return size;
+    }
+
+    //setter for array size
+    public void setSize(int size) {
+        this.size = size;
+    }
 
 }
